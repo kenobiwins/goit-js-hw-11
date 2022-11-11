bdda4b25d4c525cbf129c8bb9adbad28ba9ba326
Max Sidorenko <fivemoshr@gmail.com>
2022-11-11 10:38:32 +0200

    Add alternative colution for infinite scroll

--- src/js/infinite-scroll.js
+++ src/js/infinite-scroll.js
@@ -7,13 +7,21 @@ window.addEventListener('scroll', debounce(checkScrollPosition, 500));
 window.addEventListener('resize', throttle(checkScrollPosition, 500));
 
 async function checkScrollPosition(e) {
-  const heightBody = document.body.offsetHeight;
-  const viewPortHeight = window.innerHeight;
-  const scrolled = window.scrollY;
-  const trigger = heightBody - viewPortHeight / 4;
-  const position = scrolled + viewPortHeight;
-  if (position >= trigger) {
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+  if (scrollHeight - scrollTop <= clientHeight) {
+    // console.log('scrollHeight', scrollHeight);
+    // console.log('scrollTop', scrollTop);
+    // console.log('clientHeight', clientHeight);
     loadMore();
     hideTitle();
   }
+  // const heightBody = document.body.offsetHeight;
+  // const viewPortHeight = window.innerHeight;
+  // const scrolled = window.scrollY;
+  // const trigger = heightBody - viewPortHeight / 4;
+  // const position = scrolled + viewPortHeight;
+  // if (position >= trigger) {
+  //   loadMore();
+  //   hideTitle();
+  // }
 }
